b01bf02f35dbbf307d35b2f182ac663bb83f5522
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-04-01 00:00:46 +0100

    feat(Course): add Outline()

--- src/course/course.py
+++ src/course/course.py
@@ -24,6 +24,8 @@ class Course(): # pylint: disable=too-few-public-methods
         
         if (not page_type) or page_type["content"] != "DPT":
            raise ValueError('Page type is not DPT') 
+           
+        self.Outline()   
             
             
     def Code(self):
@@ -45,8 +47,32 @@ class Course(): # pylint: disable=too-few-public-methods
             kw.append(k.strip())
             
         return kw    
+    
+    def Outline(self):
+        #return number of credits
+        for caption in self.Soup.find_all('caption'):
+            if caption.get_text() == 'Course Outline':
+                table = caption.find_parent('table', {'class': 'sitstablegrid'})
+        
+        data = []
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            data.append([ele for ele in cols if ele]) # Get rid of empty values
+            
+        self.Outline = data
+        self.School = data[0][1]
+        self.College = data[0][3]
+        self.Credit_level = data[1][1]
+        self.Availability = data[1][3]
+        self.SCQF_credits = int(data[2][1])
+        self.ECTS_credits = int(data[2][3])
+        self.Summary = data[4][1]
+        self.Description = data[5][1]
+    
+    def Activities(self):
+        #return activities and hours
+        return None
 
-
-
-
-
+    
--- src/course/test_course.py
+++ src/course/test_course.py
@@ -27,8 +27,26 @@ class TestCourse(unittest.TestCase):
         
         keywords = ["Control Systems", "Control Engineering", "Instrumentation"]
         self.assertEqual(c.Keywords(), keywords)
-
-
+        
+        self.assertEqual(c.School,"School of Engineering")
+        
+        self.assertEqual(c.College, 'College of Science and Engineering')
+        
+        self.assertEqual(c.Credit_level, 'SCQF Level 9 (Year 3 Undergraduate)')
+        
+        self.assertEqual(c.Availability, 'Available to all students')
+        
+        self.assertEqual(c.SCQF_credits, 10)
+        
+        self.assertEqual(c.ECTS_credits, 5)
+        
+        summary = 'This is a first course in the design and analysis of instrumentation and control systems. The course starts with an introduction to instrumentation, covering the basics of sensor technology and measurement techniques, including the characteristics and real-world limitations of transducers as well as their interfacing with the control system. It then goes on to introduce Control Theory, providing a basic understanding and building the mathematical background for the modelling, design and analysis of linear single-input single-output feedback systems. It then introduces the concept of stability as well as the available methods for its assessment. It develops the analytical tools for the design of appropriate controllers to improve system performance. It allows students to appreciate the interdisciplinary nature and universal application of control engineering. Finally it introduces modern approaches including application of artificial intelligence to control systems. \n\nThe course also has a an interactive lab component which allows the students to get practical experience in working with a dynamic system and designing a simple controller.'
+        
+        self.assertEqual(c.Summary, summary)
+        
+        description = 'Topics covered (and indicative no. of lectures for each):\nInstrumentation (3 lectures): main types of transducers including flow, pressure, temperature, position, force, velocity and acceleration transducers; signal conditioning and interfacing.\n\nMathematical Models of Dynamic Systems (5 lectures): \nopen and closed-loop systems; static and dynamic response; modelling of linear systems; linearisation; Laplace transform; transfer functions; block diagrams.\n\nFeedback Systems (5 lectures): \nerror signals; sensitivity; disturbance rejection; steady-state and transient response; performance of 1st and 2nd order systems; stability; Routh-Hurwitz stability criterion. \n\nControl Systems in Frequency Domain (5 lectures): Bode plots; gain and phase margins; frequency domain performance specifications; relative stability; controller design using frequency response methods.\n\nController Design (4 lectures):\nProportional-Integral-Derivative controllers; Phase-lead and lag compensators; introduction to Artificial Intelligence for Control (Neural Networks, Fuzzy Controllers).'
+        
+        self.assertEqual(c.Description, description)
 
 if __name__ == "__main__":
     unittest.main()
